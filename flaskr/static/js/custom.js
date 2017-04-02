a3446d5b57e53a1b1450ffd02255049dd0701548
Jacob <jacobgoldberg22@gmail.com>
2017-04-02 11:45:35 -0400

    field value filled

--- flaskr/static/js/custom.js
+++ flaskr/static/js/custom.js
@@ -103,6 +103,10 @@ function initMap() {
                 pos.lng = lng;
                 marker.position = pos;
                 marker.draggable = false;
+                $('#longitude').val(lng);
+                $('#latitude').val(lat);
+                // $('#eventType').val(); NEEDS TO BE FILLED
+                $('#timestamp').val(timeStamp());
             });
 
 
@@ -137,3 +141,46 @@ function initMap() {
             'Error: The Geolocation service failed.' :
             'Error: Your browser doesn\'t support geolocation.');
     }
+
+
+
+    /**
+ * Return a timestamp with the format "m/d/yy h:MM:ss TT"
+ * @type {Date}
+ * credit to https://gist.github.com/hurjas/2660489
+ */
+
+function timeStamp() {
+// Create a date object with the current time
+  var now = new Date();
+
+// Create an array with the current month, day and time
+  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+
+// Create an array with the current hour, minute and second
+  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+
+// Determine AM or PM suffix based on the hour
+  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+
+// Convert hour from military time
+  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+
+// If hour is 0, set it to 12
+  time[0] = time[0] || 12;
+
+// If seconds and minutes are less than 10, add a zero
+  for ( var i = 1; i < 3; i++ ) {
+    if ( time[i] < 10 ) {
+      time[i] = "0" + time[i];
+    }
+  }
+
+// Return the formatted string
+  return date.join("/") + " " + time.join(":") + " " + suffix;
+}
+
+$('#submit-button').popover({
+   'placement':'top',
+   'content':'Drag the red marker and click me to confirm your location'
+}).popover('show');
